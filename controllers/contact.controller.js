055fd7736af4bc005125ffef8d3d7b6280897f06
indrapradesa <indrapradesa77@gmail.com>
2024-06-19 15:17:50 +0700

    feat:fixed

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,14 +1,27 @@
 import { google } from "googleapis";
+import { authorize } from "../controllers/auth.controller.js";
 import { phoneNumberFormater } from "../utils/formatedNumber.js";
 
-async function searchContact({ auth, phoneNumber }) {
+async function searchContact({ name, phoneNumber }) {
   try {
+    const auth = await authorize();
     const service = google.people({ version: "v1", auth });
+
     const res = await service.people.searchContacts({
       query: phoneNumber,
       readMask: "phoneNumbers",
     });
 
+    if (!res) {
+
+      const results = await service.people.searchContacts({
+        query: name,
+        readMask: "names,phoneNumbers",
+      });
+      console.log("Name" + results);
+      return results.data.results
+    }
+    console.log("phoneNumbers" + res);
     return res.data.results;
   } catch (err) {
     throw new Error(err.message || "Internal Server Error");
@@ -26,7 +39,8 @@ export const listConnectionNames = async (req, res) => {
     const service = google.people({ version: "v1" });
     const results = await service.people.connections.list({
       resourceName: "people/me",
-      personFields: "names,phoneNumbers,emailAddresses",
+      personFields: "names,phoneNumbers,emailAddresses,clientData,imClients",
+      requestSyncToken: true,
       auth,
     });
     const connections = results.data.connections;
@@ -65,33 +79,90 @@ export const getContact = async (req, res) => {
 
 export const createContact = async (req, res) => {
   try {
-    const { auth } = req;
+    var { auth } = req;
+
     if (!auth) {
       return res.status(401).json("Authorization Required");
     }
 
-    const { name, email = "", phoneNumber } = req.body;
+    const { branch, name, email = "", phoneNumber, streetAddress, birthdays } = req.body;
     const formattedPhoneNumber = phoneNumberFormater(phoneNumber);
 
     const service = google.people({ version: "v1", auth });
     const contact = await searchContact({
       auth: auth,
-      phoneNumber: formattedPhoneNumber,
+      name: name,
+      phoneNumber: phoneNumber
     });
 
-    if (contact) {
-      return res.status(409).json({ error: "Number already exists!" });
+    if (Array.isArray(contact)) {
+      const numbers =[];
+
+      const promises = contact.map(async (data) => {
+        const results = await service.people.get({
+          resourceName: data.person.resourceName,
+          personFields: "names,emailAddresses,phoneNumbers",
+        });
+        numbers.push(results.data.phoneNumbers?.[0].value); // extract the phone number value
+        return results;
+      });
+  
+      const resultsArray = await Promise.all(promises);
+  
+      const found = numbers.find((element) => element === formattedPhoneNumber);
+      console.log("found: " + found); //
+      if (found) {
+        return res.status(409).json("Numbers is exists!");
+      }
+      return;
     }
+    
+    try {
 
-    const result = await service.people.createContact({
-      requestBody: {
-        phoneNumbers: [{ value: formattedPhoneNumber }],
-        names: [{ givenName: name }],
-        emailAddresses: [{ value: email }],
-      },
-    });
+      const group = 
+      {
+        contactGroupId: 'myContacts',
+        contactGroupResourceName: 'contactGroups/myContacts'
+      }
 
-    res.status(200).json("Contact created successfully");
+      if ( branch == 1) {
+  
+        await service.people.createContact({
+          requestBody: {
+            phoneNumbers: [{ value: formattedPhoneNumber, type: 'work' }],
+            names: [{ givenName: name }],
+            emailAddresses: [{ value: email }],
+            addresses : [{ streetAddress: streetAddress }],
+            birthdays : [{ date: birthdays }],
+            memberships : [{ contactGroupMembership: group }]
+          }
+        });
+      } else if ( branch == 2 ) {
+  
+        const label = 
+        {
+          contactGroupId: '3838b2498bf088c3',
+          contactGroupResourceName: 'contactGroups/3838b2498bf088c3',
+        }
+  
+        await service.people.createContact({
+          requestBody: {
+            phoneNumbers: [{ value: formattedPhoneNumber, type: 'work' }],
+            names: [{ givenName: name }],
+            emailAddresses: [{ value: email }],
+            addresses : [{ streetAddress: streetAddress }],
+            birthdays : [{ date: birthdays }],
+            memberships : [{ contactGroupMembership: group, contactGroupMembership: label }]
+          }
+        });
+      }
+
+      res.status(200).json("Contact created successfully");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json({ err: "Internal Server Error" });
+    }
+    
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ error: "Internal Server Error" });
@@ -99,39 +170,58 @@ export const createContact = async (req, res) => {
 };
 
 export const updateContact = async (req, res) => {
-  const { oldNumber, newNumber } = req.body;
+  const auth = await authorize();
+  const { name, oldNumber, newNumber } = req.body;
   const newNumberFormatted = phoneNumberFormater(newNumber);
   const oldNumberFormatted = phoneNumberFormater(oldNumber);
-  try {
-    const { auth } = req;
-    if (!auth) {
-      return res.status(401).json("Authorization Required");
-    }
-    const service = google.people({ version: "v1", auth });
-    const contact = await searchContact({
-      auth: auth,
-      phoneNumber: oldNumberFormatted,
+
+  const service = google.people({ version: "v1", auth });
+  const contact = await searchContact({
+    name,
+    phoneNumber: oldNumberFormatted,
+  });
+
+  if (Array.isArray(contact)) {
+    const numbers = [];
+
+    const promises = contact.map(async (data) => {
+      const results = await service.people.get({
+        resourceName: data.person.resourceName,
+        personFields: "names,emailAddresses,phoneNumbers",
+      });
+      numbers.push(results.data.phoneNumbers?.[0].value); // extract the phone number value
+      return results;
     });
 
-    if (!contact) {
-      return res.status(404).json({ error: "Number do not exists!" });
-    }
+    const resultsArray = await Promise.all(promises);
 
+    var found = numbers.find((element) => element === oldNumberFormatted);
+  }
+
+  if (!found) {
+    return res.status(409).json({ error: "Number does not exist!" });
+  }
+
+  try {
     const id = contact[0].person.resourceName;
-    const etag = contact[0].person.etag;
+    let etag = contact[0].person.etag;
 
-    await service.people.updateContact({
+    const updateFields = {
       resourceName: id,
       personFields: "phoneNumbers",
       updatePersonFields: "phoneNumbers",
       resource: {
-        etag: etag,
+        etag,
         phoneNumbers: [{ value: newNumberFormatted }],
       },
-    });
+    };
+
+    await service.people.updateContact(updateFields);
+
     res.status(200).json("Contact updated successfully");
+
   } catch (error) {
-    console.log(error.message);
+    console.error(`Error updating contact: ${error.message}`);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -7,15 +7,17 @@ import {
 } from "../controllers/contact.controller.js";
 import { protectCreate, protectUpdate } from "../middleware/protectRoute.js";
 import rateLimit from "express-rate-limit";
+import apicache from 'apicache';
 
 const router = express.Router();
+let cache = apicache.middleware
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
   max: 10,
 });
 
-router.get("/", listConnectionNames);
-router.get("/:id", getContact);
+router.get("/", cache('30 second'), listConnectionNames);
+router.get("/:id", cache('30 second'), getContact);
 router.post("/create", protectCreate, limiter, createContact);
 router.post("/update", protectUpdate, limiter, updateContact);
 
